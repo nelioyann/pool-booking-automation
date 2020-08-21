ab3a7ca4eee5bb50aa4857e4ac68c482fa7f5ef4
nelioyann <nanayannick1998@gmail.com>
2020-08-21 06:12:05 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from time import sleep
+from selenium import webdriver
+import os
+from selenium.webdriver.common.by import By
+# keyboard = Controller()
+from selenium.webdriver.support.ui import WebDriverWait
+
