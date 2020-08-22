be21cbc3a1af1df5db71425af6276980bed92e46
nelioyann <nanayannick1998@gmail.com>
2020-08-22 10:01:42 +0200

    minimal working version

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Pool Booking
+# Pool Time
+
+Automating my Pool reservation because it is not practical to check the website expecting for a new slot to become available.
+This script check the available slots with my availability and send me what works for me by message using the Slack API.
--- main.py
+++ main.py
@@ -1,44 +1,112 @@
 from time import sleep
 from selenium import webdriver
 import os
+from datetime import datetime
 from selenium.webdriver.common.by import By
+from slack import WebClient
+from slack.errors import SlackApiError
+from secret import SLACK_API_TOKEN, URL, CHANNEL_ID
+
 # keyboard = Controller()
 from selenium.webdriver.support.ui import WebDriverWait
 
-
-url = "https://reservations.lille.fr/event/piscines"
-
-browser = webdriver.Chrome("./driver/chromedriver")
+client = WebClient(token=SLACK_API_TOKEN)
 
 
+def slack_message(message):
+    try:
+        response = client.chat_postMessage(
+            channel=CHANNEL_ID,
+            text=message
+        )
+    except SlackApiError as e:
+        assert e.response["error"]
 
+
+
+openDays = ["20200824", "20200825", "20200826", "20200827", "20200828",
+            "20200901", "20200902", "20200903", "20200904"]
+
+# openHours = ["15:15", "16:30","17:45" ]
+openHours = ["16:30", "18:00"]
 # Patters
-showMore = '/html/body/div[1]/div[3]/div[2]/div[1]/div/div/button'
-bookButton = '/html/body/div[1]/div[4]/div[3]/div/a[1]'
-homeLink = '/html/body/nav/div/a[1]/h3'
-locationSelect = '/html/body/div[1]/div[4]/div[3]/div/div/div/form/div[2]/div[1]/select'
-mylocation = '/html/body/div[1]/div[4]/div[3]/div/div/div/form/div[2]/div[1]/select/option[2]'
+showMoreSelector = '/html/body/div[1]/div[3]/div[2]/div[1]/div/div/button'
+bookingSelector = '/html/body/div[1]/div[4]/div[3]/div/a[1]'
+locationSelector = '/html/body/div[1]/div[4]/div[3]/div/div/div/form/div[2]/div[1]/select/option[2]'
+datesSelector = '#dates option:not([disabled])'
+hoursSelector = '#heures option:not([disabled]'
+delay = 2
 
-try:
-    browser.get(url)
-    # password_field = browser.find_element(By.XPATH, '//*[@id="password"]')
-    sleep(2)
-    poolElement =  browser.find_element(By.XPATH, showMore)
-    sleep(2)
-    print(poolElement)
-    poolElement.click()
-    sleep(2)
-    bookingElement =  browser.find_element(By.XPATH, bookButton)
-    sleep(2)
-    print(bookingElement)
-    bookingElement.click()
 
-    sleep(2)
-    locationElement =  browser.find_element(By.XPATH, mylocation)
-    sleep(2)
-    print(locationElement)
-    locationElement.click()
+while True:
+    browser = webdriver.Chrome("./driver/chromedriver")
+    print("Browser lauched successfully")
+    browser.set_window_size(600, 768)
+    os.system("cls")
 
-finally:
-    sleep(10)
-    browser.quit()
+    try:
+        # Open the target URL
+        slack_message("===Checking available pool slots===")
+        messages = []
+
+        print("Phone notified")
+
+        browser.get(URL)
+        sleep(delay)
+        print("Website launched")
+        # Select a pool
+        poolElement = browser.find_element(By.XPATH, showMoreSelector)
+        sleep(delay)
+        poolElement.click()
+        print("Pool selected \n")
+        sleep(delay)
+
+        # Click to start the reservation process
+        bookingElement = browser.find_element(By.XPATH, bookingSelector)
+        sleep(delay)
+        bookingElement.click()
+        sleep(delay)
+        print("Exploring available slots")
+
+        # Select the right municipality
+        locationElement = browser.find_element(By.XPATH, locationSelector)
+        sleep(delay)
+        locationElement.click()
+
+        # Find available the days slot
+        sleep(delay)
+        dateElements = browser.find_elements(By.CSS_SELECTOR, datesSelector)
+        sleep(delay)
+        for dateElement in dateElements:
+            dateElement.click()
+            # Filter the ones that concord with current availabity
+            date = dateElement.get_attribute("value")
+            if (date in openDays):
+                    time = datetime(int(date[:4]), int(
+                        date[4:6]), int(date[6:8])).strftime("%B %d")
+                    print(time)
+                    # print(date[:4], date[4:6], date[6:8])
+                    # print(date)
+                    sleep(delay)
+                    # Select hours slots
+                    hourElements = browser.find_elements(
+                        By.CSS_SELECTOR, hoursSelector)
+                    for hourElement in hourElements:
+                        # print(hourElement)
+                        hour = hourElement.get_attribute("value")
+                        if (hour in openHours):
+                            messages.append(f'the {hour} slot is available on {time}')
+                            print(f'Available on {time} at {hour}')
+    except:
+        print("an error occured")
+
+    finally:
+        sleep(5)
+        browser.quit()
+
+    print("\n Sending notifications available slots on Slack")
+    for message in messages:
+        slack_message(message)
+
+    print("Next research in 30mn")
+    sleep(1800)
